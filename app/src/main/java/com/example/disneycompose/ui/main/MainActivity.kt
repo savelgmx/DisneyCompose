3105d4f9185ee60e040c490b0fdd2c362a210e0f
savelgmx <savel@gmx.com>
2022-08-17 16:14:53 +0700

    last strikes

--- app/src/main/java/com/example/disneycompose/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/disneycompose/ui/main/MainActivity.kt
@@ -2,11 +2,33 @@ package com.example.disneycompose.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.disneycompose.R
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.annotation.VisibleForTesting
+import androidx.compose.runtime.CompositionLocalProvider
+import com.example.disneycompose.ui.root.RootViewModel
+import com.example.disneycompose.ui.theme.DisneyComposeTheme
+import com.skydoves.landscapist.coil.LocalCoilImageLoader
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+  @VisibleForTesting
+  internal val viewModel: RootViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+    setContent {
+      CompositionLocalProvider(LocalCoilImageLoader provides viewModel.imageLoader) {
+
+        DisneyComposeTheme {
+
+          DisneyMainScreen()
+        }
+      }
+    }
     }
 }
---
+++ app/src/main/java/com/example/disneycompose/ui/root/RootViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.disneycompose.ui.root
+
+
+import androidx.lifecycle.ViewModel
+import coil.ImageLoader
+import dagger.hilt.android.lifecycle.HiltViewModel
+import timber.log.Timber
+import javax.inject.Inject
+
+@HiltViewModel
+class RootViewModel @Inject constructor(
+    val imageLoader: ImageLoader
+) : ViewModel() {
+
+    init {
+        Timber.d("init RootViewModel")
+    }
+}
